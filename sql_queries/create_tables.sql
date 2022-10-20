8a81daee2c45daf0b8eec8488209ab3e64246bd3
newtiiled <47162985+newtiiled@users.noreply.github.com>
2022-10-20 19:36:24 -0400

    Create create_tables.sql

---
+++ sql_queries/create_tables.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE app;
+USE app;
+
+CREATE TABLE Books(bookID INT NOT NULL PRIMARY KEY, 
+                    title CHAR(100) NOT NULL, 
+                    author CHAR(200) NOT NULL, 
+                    description TEXT, 
+                    genres CHAR(200), 
+                    publishDate CHAR(20));
+
+CREATE TABLE Users(userID INT NOT NULL PRIMARY KEY, 
+                    username CHAR(200) NOT NULL UNIQUE, 
+                    userPassword CHAR(100) NOT NULL, 
+                    firstName CHAR(200) NOT NULL, 
+                    favBook char(200), 
+                    favGenre CHAR(200));
+
+CREATE TABLE ToRead(userID INT NOT NULL REFERENCES Users(userID), 
+                    bookID INT NOT NULL NULL REFERENCES Books(bookID),
+                    PRIMARY KEY(userID, bookId));
+
+CREATE TABLE DidRead(userID INT NOT NULL REFERENCES Users(userID), 
+                    bookID INT NOT NULL REFERENCES Books(bookID), 
+                    rating INT, 
+                    dateFinished CHAR(20),
+                    PRIMARY KEY(userID, bookId));
